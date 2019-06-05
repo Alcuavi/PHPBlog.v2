1323d2e518b9d7bd5664078b177d290c369e50cd
Alberto Cuadrado <Alberto.cuvi@gmail.com>
2019-06-05 10:29:37 +0200

    Finish project structure and finish Twig

---
+++ src/ViewManager.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace App;
+
+use twig;
+
+class ViewManager
+{
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__).'\templates');
+        $this -> twig = new \Twig\Enviroment($loader, [
+            'cache' => dirname(__DIR__).'\cache\views'
+        ]);
+    }
+
+    public function render ($view, $args=[])
+    {
+        if($args !=null)
+        {
+            extract ($args, EXTR_SKIP);
+        }
+
+        $file = dirname(__DIR__)."/templates/".$view;
+
+        if (is_readeable($file))
+        {
+            require $file;
+        } else 
+        {
+            throw new \InvalidArgumentException();
+        }
+    }
+
+    public function renderTemplate ($template, $args=[])
+        {
+            echo $this -> twig -> render($template, $args);
+        }
+}
--- src/kernel.php
+++ src/kernel.php
@@ -7,5 +7,7 @@ class kernel
     public function __construct()
     {
         echo "<h1> Pemba 1 </h1>";
+        $viewManager = new ViewManager();
+        $viewManager -> renderTemplate("index.view.html");
     }
 }
